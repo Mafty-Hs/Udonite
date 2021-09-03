6719a57635bf97c680ee2885261b71f7b508f35e
Mafty-Hs <theo0083@hotmail.co.jp>
2021-09-03 19:33:00 +0900

    不要なもの削除

--- src/assets/d.ts/asset-shapes.d.ts
+++
@@ -1,7 +0,0 @@
-/**
- * アセットを含むオブジェクトです。このクラスは将来変更する可能性が高いので deprecated とします。
- * 利用しないでください。
- * @type {{}}
- */
-declare var Assets: {};
-export { Assets };
--- src/assets/d.ts/data/data-shape.d.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Created by 「asset-shapes.fla」/「generate-assets.jsfl」 on Wed Jan 20 2016
- * !!!!!このコードはJSFLから自動生成されたコードです。修正する場合はご注意ください。!!!!!
- */
-export declare class ShapeData {
-    assetList: string[];
-    constructor();
-}

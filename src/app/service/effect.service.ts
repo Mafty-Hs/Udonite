8a034524448ad2fd81b27cb872c91324f8c68364
Mafty-Hs <theo0083@hotmail.co.jp>
2021-11-30 11:41:30 +0900

    微修正

--- src/app/service/effect.service.ts
+++ src/app/service/effect.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import * as THREE from 'three';
-import { Observable } from 'rxjs';
-import { Subject } from 'rxjs';
+import { Observable ,Subject } from 'rxjs';
 
 interface effectData {
       time: number;

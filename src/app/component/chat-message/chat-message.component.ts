aec2922644cfc65d84fb9ec0ffe3bad32f54be11
Mafty-Hs <theo0083@hotmail.co.jp>
2021-12-17 20:38:16 +0900

    チャットメッセージ修正

--- src/app/component/chat-message/chat-message.component.ts
+++ src/app/component/chat-message/chat-message.component.ts
@@ -5,28 +5,12 @@ import { ChatMessage } from '@udonarium/chat-message';
 import { ImageFile } from '@udonarium/core/file-storage/image-file';
 import { ChatMessageService } from 'service/chat-message.service';
 
-import { PeerCursor } from '@udonarium/peer-cursor';
-
 @Component({
   selector: 'chat-message',
   templateUrl: './chat-message.component.html',
   styleUrls: ['./chat-message.component.css'],
   animations: [
     trigger('flyInOut', [
-      transition('* => active', [
-        animate('200ms ease-out', keyframes([
-          style({ transform: 'translateX(100px)', opacity: '0', offset: 0 }),
-          style({ transform: 'translateX(0)', opacity: '1', offset: 1.0 })
-        ]))
-      ]),
-      transition('void => *', [
-        animate('200ms ease-out', keyframes([
-          style({ opacity: '0', offset: 0 }),
-          style({ opacity: '1', offset: 1.0 })
-        ]))
-      ])
-    ]),
-    trigger('flyInOutMe', [
       transition('* => active', [
         animate('200ms ease-out', keyframes([
           style({ transform: 'translateX(-100px)', opacity: '0', offset: 0 }),
@@ -67,10 +51,6 @@ export class ChatMessageComponent implements OnInit, AfterViewInit {
     if (file) this.imageFile = file;
     let time = this.chatMessageService.getTime();
     if (time - 10 * 1000 < this.chatMessage.timestamp) this.animeState = 'active';
-  }
-
-  get isMine(): boolean {
-    return this.chatMessage.isSendFromSelf;
   }
 
   ngAfterViewInit() {
